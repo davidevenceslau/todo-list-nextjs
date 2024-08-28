3b471cac6ddde3e1e43135ddb20affe88f755539
David Evaristo <david.venceslau3@allianca.com>
2024-08-28 01:51:32 -0300

    feat: favicon and adjusts

--- src/components/modais/ModalConfirm/index.tsx
+++ src/components/modais/ModalConfirm/index.tsx
@@ -56,7 +56,7 @@ export function ModalConfirm({
             </h3>
           )}
           <div className="mt-2">
-            <p className="text-sm text-gray-500 whitespace-pre-line">
+            <p className="text-sm text-gray-500 whitespace-pre-line text-[1rem]">
               {message}
             </p>
           </div>
