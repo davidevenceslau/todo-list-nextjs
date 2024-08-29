d220749b966686b55bf51fbff020a59b69ab01f7
David Evaristo <david.venceslau3@allianca.com>
2024-08-28 22:38:51 -0300

    feact: changed sort to use api version  in production

--- src/actions/fetchTasks.ts
+++ src/actions/fetchTasks.ts
@@ -5,7 +5,7 @@ import { ApiResponse, ErrorApi } from "@/api/types";
 
 export const fetchTasks = async (): Promise<ApiResponse> => {
   try {
-    const response = await fetch(`${API}?_sort=-createDate`);
+    const response = await fetch(`${API}?_sort=createDate&_order=desc`);
     if (!response.ok) throw new Error(apiMessages.error.fetchTasks);
     const data = (await response.json()) as Task[];
     return { data, success: true };
